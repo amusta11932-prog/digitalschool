e4eba9ac965788f7c7e5a0b49b0a7d0a2425c164
amusta11932-prog <a.musta11932@gmail.com>
2026-07-16 10:29:37 +0200

    Ai Website Build

---
+++ script.js
@@ -0,0 +1,6 @@
+// AI Learning Hub - Interactive Features
+// For students aged 12-18
+
+document.querySelector('.btn').addEventListener('click', () => {
+    document.querySelector('.hero').scrollIntoView({ behavior: 'smooth', block: 'end' });
+});
